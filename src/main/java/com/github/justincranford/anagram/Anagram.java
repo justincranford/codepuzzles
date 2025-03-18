da5adacaaee0f82c35bd7b7f4c1b6f2bf683b527
Justin Cranford <justincranford@hotmail.com>
2025-03-18 11:25:32 -0400

    Add "Counting Sort" comment in Anagram.serializedHistogram

--- src/main/java/com/github/justincranford/anagram/Anagram.java
+++ src/main/java/com/github/justincranford/anagram/Anagram.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public final class Anagram {
     private Anagram() { }
@@ -27,13 +26,26 @@ public final class Anagram {
         return new ArrayList<>(grouped.values());
     }
 
-
+    /**
+     * Uses a variation of <A HREF="https://en.wikipedia.org/wiki/Counting_sort">Counting Sort</A> to compute an ordered histogram of
+     * the unique characters in a string. Caller can use equals or hashCode to compare words and see if they are anagrams of each other.
+     * <P>
+     * It works by creating an ordered map of unique characters and their counts; the characters are handled as Unicode code points (aka UTF32).
+     * The code points and their counts are serialized as a CSV string.
+     * The CSV is a deterministic-ordered histogram of the unique characters of the string and their counts.
+     * <P>
+     * The histogram approach is better than the naive approach of converting to char[] and sorting that array.
+     * <UL>Naive sort of value.toCharArray() doesn't deduplicate the characters, so worst case timing is higher than sorting unique characters.
+     * <UL>Naive sort of UTF-16 code units corrupts the order of surrogate pairs, using Unicode code points (aka UTF32) preserves the characters of the string.
+     * <P>
+     * @param value Non-null string of arbitrary length. Handles empty or whitespace strings.
+     * @return Ordered CSV histogram representing the unique characters and their corresponding counts.
+     */
     public static String serializedHistogram(final String value) {
-        final Map<Integer, Integer> codePointCounts = new TreeMap<>(); // deterministic order
-        value.codePoints().forEach(codePoint -> {
-            codePointCounts.put(codePoint, codePointCounts.getOrDefault(codePoint, 0) + 1);
-        });
-        final List<String> csvCodePointAndCount = codePointCounts.entrySet().stream().map(e -> e.getKey() + "=" + e.getValue()).toList();
+        final Map<Integer, Integer> codePointCounts = new HashMap<>();
+        value.codePoints().forEach(codePoint -> codePointCounts.merge(codePoint, 1, Integer::sum));
+        final List<Map.Entry<Integer, Integer>> sortedEntries = codePointCounts.entrySet().stream().sorted(Map.Entry.comparingByKey()).toList();
+        final List<String> csvCodePointAndCount = sortedEntries.stream().map(e -> e.getKey() + "=" + e.getValue()).toList();
         return String.join(",", csvCodePointAndCount);
     }
 }
